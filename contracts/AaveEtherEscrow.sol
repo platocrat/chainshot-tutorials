86b9b49ff491a819cc3fc531a1fae7fccd34bb58
Jack Winfield <platocrat@tuta.io>
2021-01-08 16:03:58 -0600

    Setup contracts for step 1

--- contracts/AaveEtherEscrow.sol
+++ contracts/AaveEtherEscrow.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.7.5;
+
+import "./IERC20.sol";
+import "./IWETHGateway.sol";
+
+contract Escrow {
+    address arbiter;
+    address depositor;
+    address beneficiary;
+}
---
+++ contracts/interfaces/IWETHGateway.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.7.5;
+
+interface IWETHGateway {
+    function depositETH(address onBehalfOf, uint256 referralCode)
+        external
+        payable;
+
+    function withdrawETH(uint256 amount, address onBehalfOf) external;
+
+    function repayETH(
+        uint256 amount,
+        uint256 rateMode,
+        address onBehalfOf
+    ) external payable;
+
+    function borrowETH(
+        uint256 amount,
+        uint256 interestRateMode,
+        uint16 referralCode
+    ) external;
+}
